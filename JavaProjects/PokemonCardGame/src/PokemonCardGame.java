6c35f53975ca223dae69e1f2ae3c67bdba9cc96e
Rocco Vulpis <146395748+roccovulpis@users.noreply.github.com>
2024-03-11 16:38:25 -0400

    Add files via upload

--- JavaProjects/PokemonCardGame/src/PokemonCardGame.java
+++ JavaProjects/PokemonCardGame/src/PokemonCardGame.java
@@ -2,31 +2,47 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * The main game engine for a Pokemon card game. This class manages the game setup,
+ * including initializing players, managing turns, and determining the game outcome.
+ * 
+ * @author Rocco Vulpis
+ * @version 3/7/2024
+ */
 public class PokemonCardGame {
 
+	// Fields 
 	private final Player playerOne;
 	private final Player playerTwo;
 	private final Scanner kb;
 
 
+	/**
+	 * Constructs a new Pokemon Card Game instance. Initializes two players and a scanner for input.
+	 */	
 	public PokemonCardGame() {
 		playerOne = new Player(1);
 		playerTwo = new Player(2);
 		kb = new Scanner(System.in);
 	}
 
+	/**
+	 * Sets up the game by determining which player goes first through a coin flip and initializing decks.
+	 */
 	public void setUpGame() {
 		String choice = "";
+		// Sets each player's opponent.
 		playerOne.setOpponent(playerTwo);
 		playerTwo.setOpponent(playerOne);
+		// Loop until a valid input is provided by the user.
 		while (!choice.equals("heads") && !choice.equals("tails")) {
 			System.out.println("Heads or Tails?");
 			choice = kb.nextLine().toLowerCase(); 
-
 			if (!choice.equals("heads") && !choice.equals("tails")) {
 				System.out.println("Please enter either \"Heads\" or \"Tails\"");
 			}
 		}
+		// Determine the first player based on the coin flip and initialize both players decks.
 		if(choice.equals(flipCoin())) {
 			initializeDeck(playerOne, playerTwo);
 			playerOne.setIsFirst(true);
@@ -40,16 +56,24 @@ public class PokemonCardGame {
 		}
 	}
 
+	/**
+	 * Initializes the decks for both players ensuring that valid opening hands are drawn.
+	 * 
+	 * @param coinFlipWinner The player who won the coin flip and goes first.
+	 * @param coinFlipLoser The player who lost the coin flip and goes second.
+	 */
 	public void initializeDeck(Player coinFlipWinner, Player coinFlipLoser) {
+		// Initialize decks for both players and draw the initial hand and prize pile
 		coinFlipWinner.initializeDeck(20, 20, 20);
 		coinFlipWinner.drawHand();
 		coinFlipWinner.drawPrizePile();
 
+		// Makes sure the opening hand is valid.
 		while(!coinFlipWinner.isOpeningHandValid()) {
 			coinFlipWinner.resetDeck();
 			coinFlipWinner.drawHand();
 		}
-		
+
 		coinFlipLoser.initializeDeck(20, 20, 20);
 		coinFlipLoser.drawHand();
 		coinFlipLoser.drawPrizePile();
@@ -60,6 +84,11 @@ public class PokemonCardGame {
 		}
 	}
 
+	/**
+	 * Simulates flipping a coin to decide which player goes first.
+	 * 
+	 * @return A string "heads" or "tails" representing the outcome of the coin flip.
+	 */
 	public String flipCoin() {
 		Random random = new Random();
 		int coin = random.nextInt(2);
@@ -71,11 +100,17 @@ public class PokemonCardGame {
 		}
 	}
 
+	/**
+	 * Starts and runs the game loop until one player wins by empties their prize pile.
+	 */
 	public void run() {
+		// Set up the game.
 		setUpGame();
+		// Determine the first player based on who is set as first.
 		Player firstPlayer = new Player(0);
 		if(playerOne.isFirst()) firstPlayer=playerOne;
 		if(playerTwo.isFirst()) firstPlayer=playerTwo;
+		// Game loop continues until one players prize pile is depleted.
 		while (playerOne.getPrizePile().size() > 0 && playerTwo.getPrizePile().size() > 0){
 			firstPlayer.turn(true);
 			firstPlayer.getOpponent().turn(true);
--- JavaProjects/PokemonCardGame/src/RareCandy.java
+++ JavaProjects/PokemonCardGame/src/RareCandy.java
@@ -1,4 +1,17 @@
-
+/**
+ * Represents the Rare Candy Trainer card. 
+ * This card allows the player to speed up the evolution of a Pokemon.
+ * 
+ * This class extends the Trainer class and implements the TrainerAction interface to create the specific 
+ * playable action as follows: 
+ * 
+ * Chooses 1 Basic Pokemon in play. 
+ * If it's a Stage 2 card in the hand that evolves from that Pokemon, put that card onto the Basic Pokemon to evolve it, 
+ * skipping the Stage 1. The player can’t use this card during their first turn or on a Basic Pokemon that was put into play this turn.
+ * 
+ * @author Rocco Vulpis
+ * @version 3/7/2024
+ */
 public class RareCandy extends Trainer implements TrainerAction{
 
 	public RareCandy() {
